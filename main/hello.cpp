63280a69c0305d12ed303a5bc3c1be4f71f4571f
kshitijsharma37 <73789468+kshitijsharma37@users.noreply.github.com>
2023-02-19 17:51:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Made auto changes!!\n";
+  cout << "Nice!!\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
