0e926a0452495b0c8aab5b0974e0a455fca606ab
kshitijsharma37 <73789468+kshitijsharma37@users.noreply.github.com>
2023-02-19 17:56:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Made auto changes!!\n";
-  cout << "Nice!!\n";
+  cout << "Nice!!!\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
